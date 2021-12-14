33da33f72f83b2db60faff8aabd2eb8d8000a0a1
aplatero29 <alejandroplatero29@gmail.com>
2021-12-14 17:44:14 +0100

    Fixed showByCategory being auth only

--- app/Http/Controllers/Api/V1/CategoriaController.php
+++ app/Http/Controllers/Api/V1/CategoriaController.php
@@ -17,7 +17,7 @@ class CategoriaController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['index', 'show']]);
+        $this->middleware('auth:api', ['except' => ['index', 'show', 'showByCategory']]);
     }
 
     /**
